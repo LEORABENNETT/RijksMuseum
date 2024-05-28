1f2ab622f6f59a2ebfb9bfb1637841d33855e70e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-28 12:29:00 -0400

    fixed currentCollection

--- src/main/java/bennett/rijksmuseum/json/CurrentCollection.java
+++ src/main/java/bennett/rijksmuseum/json/CurrentCollection.java
@@ -1,17 +1,14 @@
 package bennett.rijksmuseum.json;
 
-
 import bennett.rijksmuseum.json.ArtObjects;
 
 public class CurrentCollection {
-    private ArtObjects[] artObjects;
 
     public ArtObjects[] getArtObjects() {
         return artObjects;
     }
 
-    public void setArtObjects(ArtObjects[] artObjects) {
-        this.artObjects = artObjects;
-    }
-}
+    ArtObjects [] artObjects;
 
+
+}
